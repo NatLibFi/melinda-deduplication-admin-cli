5fa9c2dd2a4cf569748bbdbfd5eebf1e790cef6a
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-08-11 12:56:42 +0300

    Adds record reading from file

--- src/index-record.js
+++ src/index-record.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
+const fs = require('fs');
 const params = require('commander');
 const _ = require('lodash');
 const fetch = require('node-fetch');
 const MarcRecord = require('marc-record-js');
 const marc_record_converters = require('marc-record-converters');
+const Serializers = require('marc-record-serializers');
+const debug = require('debug')('cli');
 
 const CONVERTERS = {
   'aseq': marc_record_converters.alephSequential.to,
@@ -71,12 +74,99 @@ params
   });
 
 params
-  .command('add <filename>')
+  .command('add <base> <filename>')
   .description('execute the given remote cmd')
-  .option('-f, --format [format]', 'Format of records in file')
-  .action(function(filename, options){
+  .option('-f, --format [format]', 'Format of records in file. Default is aseq')
+  .action(function(base, filename, options){
     var format = options.format || 'aseq';
-    console.log('reading records from %s as %s', filename, format);
+    if (format !== 'aseq') {
+      const availableFormats = 'aseq';
+      console.error(`Invalid record format: '${format}'. Available formats are: ${availableFormats}`);
+      process.exit(1);
+    }
+    
+    console.log('adding records from %s as %s into %s', filename, format, base);
+
+    const fileStream = fs.createReadStream(filename);
+    const reader = new Serializers.AlephSequential.Reader(fileStream);
+
+    const queue = createQueue();
+
+    reader.on('data', async function(record) {
+      fileStream.pause();
+
+      queue.add(record);
+      process.nextTick(queue.handleQueuedTasks);
+      queue.onEmpty(() => fileStream.resume());
+    });
+
+    reader.on('end', function() {
+      console.log('Done.');
+    });
+
+    reader.on('error', function(error) {
+      console.error(error);
+    });
+
+    function createQueue() {
+      let tasks = [];
+      let isRunning = false;
+      let onEmptyCallback;
+      
+      const handleQueuedTasks = async () => {
+        if (isRunning) return;
+        isRunning = true;
+        for (const record of tasks) {
+          try {
+            await triggerRecordSave(record);
+          } catch(error) {
+            console.log(error.message);
+            console.log(record.toString());
+          }
+        }
+        tasks = [];
+        isRunning = false;
+        
+        if (onEmptyCallback) {
+          onEmptyCallback.call();
+        }
+      };
+      const add = (task) => tasks.push(task);
+      const onEmpty = (fn) => onEmptyCallback = fn;
+
+      return {
+        add,
+        handleQueuedTasks,
+        onEmpty
+      };
+    }
+    
+    async function triggerRecordSave(record) {
+  
+      const recordId = _.get(record.fields.find(field => field.tag === '001'), 'value');
+      if (recordId === undefined) {
+        throw new Error('Cannot add records without 001 field.');
+      }
+      const url = `${DEDUPLICATION_API}/record/add/${base}/${recordId}`;
+      
+      const response = await fetch(url, {
+        method: 'POST', 
+        body: JSON.stringify(record),
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+      if (response.status === 200) {
+        console.log(`Saved record ${base}/${recordId}`);
+        debug(`Saved record ${base}/${recordId}`);
+      } else {
+        console.error(`Error: ${response.statusText}`);
+        const body = await response.text();
+        console.log(body);
+        process.exit(1);
+      }
+      
+    }
+
   });
 
 params.parse(process.argv);
